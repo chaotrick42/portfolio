03c4d9a2d52b9226e19eb9ed12af1908bf7f98b9
DatRick8P15 <p.parthen@googlemail.com>
2022-12-21 14:38:27 +0100

    use firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import reportWebVitals from "./reportWebVitals";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCT2TcAfPTlK3HvrTHlgKOnY2CSsmEtxbQ",
+  authDomain: "chaotrick42.firebaseapp.com",
+  projectId: "chaotrick42",
+  storageBucket: "chaotrick42.appspot.com",
+  messagingSenderId: "445489767405",
+  appId: "1:445489767405:web:e05b2d48a2f1039aff7de4",
+  measurementId: "G-EKC70DE758"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +30,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+// Initialize Firebase
